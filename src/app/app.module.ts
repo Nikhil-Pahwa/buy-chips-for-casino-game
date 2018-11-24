5df24f17c53371f079297ab50544491ad7b63ce5
Nikhil Pahwa <nikhil.pahwa@nagarro.com>
2018-11-24 19:41:22 +0530

    component selection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PackageSelectionComponent } from './package-selection/package-selection.component';
+import { PaymentMethodComponent } from './payment-method/payment-method.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'package-selection', component: PackageSelectionComponent },
+  { path: 'payment-method', component: PaymentMethodComponent },
+  { path: '', redirectTo: '/package-selection', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PackageSelectionComponent,
+    PaymentMethodComponent,
+    PageNotFoundComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
